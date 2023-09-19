c8147d8eba730b04afe5f9a598d4bb1797324a53
Hani Kaabi <hani@hnkb.de>
2023-09-19 10:51:44 +0200

    chat win

--- chat.cpp
+++ chat.cpp
@@ -5,18 +5,62 @@
 
 void chat(Socket& sock)
 {
+	HANDLE sources[2];
+
+	const int ix_socket = 0;
+	const int ix_stdin = 1;
+
+	sources[ix_socket] = WSACreateEvent();
+	if (sources[ix_socket] == WSA_INVALID_EVENT)
+		throw Socket::lastError();
+
+	if (WSAEventSelect(sock, sources[ix_socket], FD_READ | FD_CLOSE))
+		throw Socket::lastError();
+
+	//auto result = WaitForMultipleObjectsEx(1, sources, FALSE, INFINITE, FALSE);
+	//std::cout << "Event triggered " << result << "\n";
+
+	sources[ix_stdin] = GetStdHandle(STD_INPUT_HANDLE);
+
 	char buffer[1024];
 
 
-	const HANDLE sources[] { GetStdHandle(STD_INPUT_HANDLE), WSACreateEvent() };
-	WSAEventSelect(sock, sources[1], FD_READ | FD_OOB | FD_CLOSE);
+	//recv(sock, buffer, sizeof(buffer), 0);
 
-	for (int i = 0; i < 3; i++)
+
+	//const HANDLE sources[] { GetStdHandle(STD_INPUT_HANDLE), WSACreateEvent(), INVALID_HANDLE_VALUE };
+	//WSAEventSelect(sock, sources[1], FD_READ);
+
+	//INPUT_RECORD r[512];
+	//DWORD read;
+	//ReadConsoleInput(sources[ix_stdin], r, 512, &read);
+	//printf("Read: %d\n", read);
+
+	for (int i = 0; i < 13; i++)
 	{
-		auto result = WaitForMultipleObjectsEx(2, sources, FALSE, INFINITE, FALSE);
+		auto result = WaitForMultipleObjects(1, sources, FALSE, INFINITE);
 
 		std::cout << "Return value " << result << "\n";
-		fgets(buffer, sizeof(buffer), stdin);
+
+		if (result == WAIT_OBJECT_0 + ix_socket)
+		{
+			auto received = recv(sock, buffer, sizeof(buffer), 0);
+			buffer[received] = 0;
+			std::cout << " [other]: " << buffer << "\n";
+			// WSAResetEvent(sources[result]);
+		}
+
+		if (result == WAIT_OBJECT_0 + ix_stdin)
+		{
+			//fgets(buffer, sizeof(buffer), stdin);
+			INPUT_RECORD r[512];
+			DWORD read;
+			ReadConsoleInput(sources[ix_stdin], r, 512, &read);
+			printf("Read: %d\n", read);
+		}
+
+
+		WSAResetEvent(sources[ix_socket]);
 	}
 
 #if 0
